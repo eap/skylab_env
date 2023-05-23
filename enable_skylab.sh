7a4879626ce1a396c50905131c86b7a9f0e0cad2
Evan Parker <eparker@ucar.edu>
2023-05-23 13:21:57 -0600

    ewok stuff

--- enable_skylab.sh
+++ enable_skylab.sh
@@ -64,83 +64,79 @@ export PS1="\033[1;34m(skylab-dev)\033[0m $(cut -c 8- <<< $PS1)"
 
 #!/bin/bash
 
-# Set JEDI_ROOT
+if [ -d $JEDI_ROOT ]; then
+  # Set host name for R2D2/EWOK
 
-if [ -z $JEDI_SRC ] || [ -z $JEDI_ROOT ]; then
-  export JEDI_ROOT=/Users/eparker/git/jedi-bundle
-  export JEDI_SRC=/Users/eparker/git/jedi-bundle
-fi
+  # On Orion:
+  #export R2D2_HOST=orion
+  # On Discover:
+  #export R2D2_HOST=discover
+  # On Cheyenne:
+  #export R2D2_HOST=cheyenne
+  # On S4:
+  #export R2D2_HOST=s4
+  # On AWS Parallel Cluster
+  #export R2D2_HOST=aws-pcluster
+  # On your local machine / AWS single node
+  unset R2D2_HOST
 
-# Set host name for R2D2/EWOK
+  if [ -z $JEDI_BUILD ]; then
+    export JEDI_BUILD=${JEDI_ROOT}/build
+  fi
 
-# On Orion:
-#export R2D2_HOST=orion
-# On Discover:
-#export R2D2_HOST=discover
-# On Cheyenne:
-#export R2D2_HOST=cheyenne
-# On S4:
-#export R2D2_HOST=s4
-# On AWS Parallel Cluster
-#export R2D2_HOST=aws-pcluster
-# On your local machine / AWS single node
-unset R2D2_HOST
+  if [ -z $EWOK_WORKDIR ]; then
+    export EWOK_WORKDIR=${JEDI_ROOT}/workdir
+  fi
 
+  if [ -z $EWOK_FLOWDIR ]; then
+    export EWOK_FLOWDIR=${JEDI_ROOT}/ecflow
+  fi
 
-if [ -z $JEDI_BUILD ]; then
-  export JEDI_BUILD=${JEDI_ROOT}/build
-fi
+  # Add ioda python bindings to PYTHONPATH
+  PYTHON_VERSION=`python3 -c 'import sys; version=sys.version_info[:2]; print("{0}.{1}".format(*version))'`
+  export PYTHONPATH="${JEDI_BUILD}/lib/python${PYTHON_VERSION}/pyioda:${PYTHONPATH}"
 
-if [ -z $EWOK_WORKDIR ]; then
-  export EWOK_WORKDIR=${JEDI_ROOT}/workdir
-fi
+  # necessary user directories for ewok and ecFlow files
+  mkdir -p $EWOK_WORKDIR $EWOK_FLOWDIR
 
-if [ -z $EWOK_FLOWDIR ]; then
-  export EWOK_FLOWDIR=${JEDI_ROOT}/ecflow
-fi
-
-# Add ioda python bindings to PYTHONPATH
-PYTHON_VERSION=`python3 -c 'import sys; version=sys.version_info[:2]; print("{0}.{1}".format(*version))'`
-export PYTHONPATH="${JEDI_BUILD}/lib/python${PYTHON_VERSION}/pyioda:${PYTHONPATH}"
-
-# necessary user directories for ewok and ecFlow files
-mkdir -p $EWOK_WORKDIR $EWOK_FLOWDIR
-
-# ecFlow vars
-myid=$(id -u ${USER})
-if [[ $myid -gt 64000 ]]; then
-  myid=$(awk -v min=3000 -v max=31000 -v seed=$RANDOM 'BEGIN{srand(seed); print int(min + rand() * (max - min + 1))}')
-fi
-export ECF_PORT=$((myid + 1500))
+  # ecFlow vars
+  myid=$(id -u ${USER})
+  if [[ $myid -gt 64000 ]]; then
+    myid=$(awk -v min=3000 -v max=31000 -v seed=$RANDOM 'BEGIN{srand(seed); print int(min + rand() * (max - min + 1))}')
+  fi
+  export ECF_PORT=$((myid + 1500))
 
 
-# The ecflow hostname (e.g. a specific login node) is different from the R2D2/EWOK general host (i.e. system) name
-host=$(hostname | cut -f1 -d'.')
-export ECF_HOST=$host
+  # The ecflow hostname (e.g. a specific login node) is different from the R2D2/EWOK general host (i.e. system) name
+  host=$(hostname | cut -f1 -d'.')
+  export ECF_HOST=$host
 
 
-if [[ x"${R2D2_HOST}" == "x" ]]; then
-  export EWOK_STATIC_DATA=${JEDI_ROOT}/static
+  if [[ x"${R2D2_HOST}" == "x" ]]; then
+    export EWOK_STATIC_DATA=${JEDI_ROOT}/static
+  else
+    case $R2D2_HOST in
+      orion)
+        export EWOK_STATIC_DATA=/work/noaa/da/role-da/static
+        ;;
+      discover)
+        export EWOK_STATIC_DATA=/discover/nobackup/projects/jcsda/s2127/static
+        ;;
+      cheyenne)
+        export EWOK_STATIC_DATA=/glade/p/mmm/jedipara/static
+        ;;
+      s4)
+        export EWOK_STATIC_DATA=/data/prod/jedi/static
+        ;;
+      aws-pcluster)
+        export EWOK_STATIC_DATA=${JEDI_ROOT}/static
+        ;;
+      *)
+        echo "Unknown host name $R2D2_HOST"
+        exit 1
+        ;;
+    esac
+  fi
 else
-  case $R2D2_HOST in
-    orion)
-      export EWOK_STATIC_DATA=/work/noaa/da/role-da/static
-      ;;
-    discover)
-      export EWOK_STATIC_DATA=/discover/nobackup/projects/jcsda/s2127/static
-      ;;
-    cheyenne)
-      export EWOK_STATIC_DATA=/glade/p/mmm/jedipara/static
-      ;;
-    s4)
-      export EWOK_STATIC_DATA=/data/prod/jedi/static
-      ;;
-    aws-pcluster)
-      export EWOK_STATIC_DATA=${JEDI_ROOT}/static
-      ;;
-    *)
-      echo "Unknown host name $R2D2_HOST"
-      exit 1
-      ;;
-  esac
+  echo "JEDI_ROOT not defined, skipping jedi and ewok setup"
 fi
