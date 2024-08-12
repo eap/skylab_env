5f8437e6bfe2afcb7cd74fcdd1c09afb0a1f251e
Evan Parker <eparker@ucar.edu>
2024-08-12 16:06:26 -0600

    Update bootstrap_ubuntu.sh
    get rid of intel_hpc option but keep for history

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -273,11 +273,13 @@ install_after_download() {
 }
 
 install_intel_hpc() {
+    echo "This does not work. Please do not use this option, use --intel instead."
+    exit 1
     # Intel compiler
-    wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor | tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
-    echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | tee /etc/apt/sources.list.d/oneAPI.list
-    apt-get update
-    apt-get install -y intel-hpckit-runtime-2023.2.0/all
+    # wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor | tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
+    # echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | tee /etc/apt/sources.list.d/oneAPI.list
+    # apt-get update
+    # apt-get install -y intel-hpckit-runtime-2023.2.0/all
 }
 
 install_intel() {
